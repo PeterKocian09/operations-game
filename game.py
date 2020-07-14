90b5fbc7606f4e437275ceced53b8eab63a2a294
P37er <63299045+PeterKocian09@users.noreply.github.com>
2020-07-14 20:08:52 +0200

    Add files via upload

--- game.py
+++ game.py
@@ -3,25 +3,19 @@ import random, time
 t = time.time()
 random.seed()
 
-a = random.randint(1, 10)
-b = random.randint(1, 10)
-
-c = a + b
-
+re = 0
 
 for x in range(1,11):
-  r = input("%s) %s + %s = \n" % (x, a , b))
   a = random.randint(1, 10)
   b = random.randint(1, 10)
+  s = random.choice(["+","-","*"])
+  r = int(input("{0}) {1} {2} {3} = \n".format(x, a, s, b)))
+  c = eval("a{0}b".format(s))
+  if r == c:
+    re += 1
+  
 
 t1 = time.time()
 tem = t1 - t
 
-re = 0
-
-if r == c:
-  re += 1
-
-print("risposte esatte: %s in %s" %(re, tem))
-
-
+print("risposte esatte: {0} in {1:.2f}".format(re, tem))
